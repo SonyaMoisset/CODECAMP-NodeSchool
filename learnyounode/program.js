409cbf7ffd380598842ae78a9028e2d2f52da91d
SonyaMoisset <sonya.moisset@gmail.com>
2016-02-14 17:45:08 +0900

    Update program.js

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,6 +1,9 @@
-var fs = require('fs');
+var fs = require('fs')
+var path = require('path')
 
-fs.readFile(process.argv[2], function (err, contents) {
-  var lines = contents.toString().split('\n').length - 1
-  console.log(lines)
+fs.readdir(process.argv[2], function (err, list) {
+  list.forEach(function (file) {
+    if (path.extname(file) === '.' + process.argv[3])
+      console.log(file)
+  })
 })
